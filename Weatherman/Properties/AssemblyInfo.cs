fec7f1e9a588038ec70dfb046ebd2896604ee987
LysitheaStar <lysithea.star@gmail.com>
2021-03-30 18:29:35 +0300

    Add assembly info

---
+++ Weatherman/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Weatherman")]
+[assembly: AssemblyDescription("Allows precise control over time and weather zone by zone or globally")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("https://github.com/Eternita-S/Weatherman")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Eternita")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a859bc9c-18c8-4f1e-84f1-f317fe2eedae")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
